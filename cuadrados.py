8842fe12f578f1c51080b9ba241db33fabc49fd0
an3shito <and.dres2294@gmail.com>
2021-05-05 00:57:56 -0600

    first commit

---
+++ cuadrados.py
@@ -0,0 +1,12 @@
+def main():
+    print "se calcularan cuadrados de numeros"
+
+    n1 = input ("ingrese un numero entero:")
+    n2 = input ("ingrese otoro numero entero:")
+
+    for t in range(n1,n2):
+        print t*t
+
+    print "Es todo por ahora"
+
+main()
